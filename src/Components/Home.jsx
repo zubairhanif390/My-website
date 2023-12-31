b3860ce363df2b8f9bf89c0e6e497d2ec584af83
Zubair Ahmed <111756376+zubairhanif390@users.noreply.github.com>
2023-12-31 23:06:18 +0500

    add all chnges

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/table.jpg";
+import image from "../images/computer.jpg";
 
 const imageAltText = "a table of laptops and notebooks";
 
